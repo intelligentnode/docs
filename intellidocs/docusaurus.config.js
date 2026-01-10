b64952a023c101099377e3ac4b2f65c1b58f0a98
Barqawiz <barqawi.88@outlook.com>
2026-01-10 21:25:31 +0000

    Update docusaurus.config.js

--- intellidocs/docusaurus.config.js
+++ intellidocs/docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
           customCss: './src/css/custom.css',
         },
         gtag: {
-          trackingID: 'G-T3RZWDEQW0',
+          trackingID: 'G-PSSTWY93JY',
           anonymizeIP: true,
         },
       }),
